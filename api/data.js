ac37a044ed3e90bf72e55c7394eccbfbf540caf5
wsammntzr077-png <wsammntzr077@gmail.com>
2026-03-18 13:33:00 +0300

    Update data.js

--- api/data.js
+++ api/data.js
@@ -34,7 +34,7 @@ async function uploadImage(base64Data, fileName) {
   return null;
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
